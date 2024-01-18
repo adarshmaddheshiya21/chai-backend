79a9cde129067bf722dab59df158b6c8e4f5fafa
aadiaditya247 <aadiaditya247@gmail.com>
2024-01-18 10:16:57 +0530

    utils handlers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from 'dotenv'
 import connectDB from './db/connectDB.js';
+import { app } from './app.js';
 dotenv.config({path: './env'})
 
 connectDB()
+.then(()=> {
+    console.log(`Server is running on port ${process.env.PORT}`)
+    app.on("error", (err)=> {
+        console.log("Connection ERRR...", err)
+    })
+})
+.catch((err)=> {
+    console.log("Server connection error: ", err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async ()=> {}
+
+
+const asyncHandler = (requestHanlder) => {
+    (req, res, next) => {
+        Promise.resolve(requestHanlder(req, res, next))
+        .catch((err)=> next(err))
+    }
+}
+
+// const asyncHandler = (fn) => async = (req, res, next)=> {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500). ({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export {asyncHandler}
